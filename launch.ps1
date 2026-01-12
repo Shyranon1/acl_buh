838f5853a1eab0fba8efadf5043bf4388e59718e
Guillaume <guillaume.janeiro6@etu.univ-lorraine.fr>
2026-01-12 13:48:27 +0100

    Fix launch script java version mismatch and classpath

--- launch.ps1
+++ launch.ps1
@@ -1,5 +1,15 @@
 Write-Host "Starting ACL Project..."
 
+# Compile Java Client (Target Java 8 for compatibility)
+Write-Host "Compiling Client..."
+# We use --release 8 because the system has JDK 21 but JRE 1.8 active in PATH
+javac --release 8 -encoding UTF-8 client/*.java
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Compilation failed!" -ForegroundColor Red
+    Pause
+    exit
+}
+
 # Start the C++ Server
 Write-Host "Launching Server..."
 Start-Process -FilePath ".\server.exe"
@@ -9,6 +19,7 @@ Start-Sleep -Seconds 2
 
 # Start the Java Client
 Write-Host "Launching Client..."
-Start-Process -FilePath "java" -ArgumentList "client.ApplicationLivraison"
+# Use -cp . explicitly to include current directory in classpath
+Start-Process -FilePath "java" -ArgumentList "-cp . client.ApplicationLivraison"
 
 Write-Host "Done! Server and Client should be running."
